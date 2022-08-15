f71cad3f6d179ccb7f601311eff41705b845d895
Brian Meek <brian@hntlabs.com>
2022-08-15 15:52:56 -0700

    Merge upstream changes, public key tests failing
    Signed-off-by: Brian Meek <brian@hntlabs.com>

--- clientapi/auth/user_interactive.go
+++ clientapi/auth/user_interactive.go
@@ -148,8 +148,6 @@ func NewUserInteractive(
 func (u *UserInteractive) IsSingleStageFlow(authType string) bool {
 	u.RLock()
 	defer u.RUnlock()
-	u.RLock()
-	defer u.RUnlock()
 	for _, f := range u.Flows {
 		if len(f.Stages) == 1 && f.Stages[0] == authType {
 			return true
@@ -219,7 +217,6 @@ func (u *UserInteractive) NewSession() *util.JSONResponse {
 		return &res
 	}
 	u.Lock()
-	u.Lock()
 	u.Sessions[sessionID] = []string{}
 	u.Unlock()
 	return u.challenge(sessionID)
@@ -266,11 +263,7 @@ func (u *UserInteractive) Verify(ctx context.Context, bodyBytes []byte, device *
 	authType := gjson.GetBytes(bodyBytes, "auth.type").Str
 
 	u.RLock()
-
-	u.RLock()
 	loginType, ok := u.Types[authType]
-	u.RUnlock()
-
 	u.RUnlock()
 
 	if !ok {
