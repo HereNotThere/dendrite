4690d8c711cf36e57a7cdefd01cc3b4b7e908d53
Tak Wai Wong <64229756+tak-hntlabs@users.noreply.github.com>
2022-10-17 14:08:04 -0700

    Figure out space vs channel from roomid (#43)
    * pass in roomserver API so that we have access to the db
    
    * interface to get db info for spaceid and channelid
    
    * determine space or channel by querying the room db
    
    * Add authorization check to the JOIN endpoint
    
    * fix lint errors

--- clientapi/routing/routing.go
+++ clientapi/routing/routing.go
@@ -76,8 +76,7 @@ func Setup(
 
 	rateLimits := httputil.NewRateLimits(&cfg.RateLimiting)
 	userInteractiveAuth := auth.NewUserInteractive(userAPI, userAPI, cfg)
-	authorization := clientApiAuthz.NewAuthorization(cfg)
-	_ = authorization // todo: use this in httputil.MakeAuthAPI
+	authorization := clientApiAuthz.NewAuthorization(cfg, rsAPI)
 
 	unstableFeatures := map[string]bool{
 		"org.matrix.e2e_cross_signing": true,
@@ -261,7 +260,12 @@ func Setup(
 				Permission: authz.PermissionRead,
 			})
 
-			logrus.Debugf("/join/%s isAllowed = %t", vars["roomIDOrAlias"], isAllowed)
+			if !isAllowed {
+				return util.JSONResponse{
+					Code: http.StatusUnauthorized,
+					JSON: jsonerror.Forbidden(""),
+				}
+			}
 
 			return JoinRoomByIDOrAlias(
 				req, device, rsAPI, userAPI, vars["roomIDOrAlias"],
@@ -347,6 +351,7 @@ func Setup(
 			if err != nil {
 				return util.ErrorResponse(err)
 			}
+
 			return SendInvite(req, userAPI, device, vars["roomID"], cfg, rsAPI, asAPI)
 		}),
 	).Methods(http.MethodPost, http.MethodOptions)
